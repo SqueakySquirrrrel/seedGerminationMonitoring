b7096de757460fbe7aaee53750a738daf65b7086
SqueakySquirrrrel <siddhrajsinh.padhiyar@gmail.com>
2022-06-29 13:31:08 -0500

    Image upload

--- Images/testScript.sh
+++ Images/testScript.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-VAR=10
+VAR=5
 while [ $VAR -gt 0 ]; do
 	#echo Value of VAR is: $VAR	
 	$(raspistill -o $VAR.jpg)
 	$(git -C /home/pi/seedGerminationMonitoring add --all)
-	$(git -C /home/pi/seedGerminationMonitoring commit -m "Image $VAR")
+	$(git -C /home/pi/seedGerminationMonitoring commit -m "Image upload")
 	$(git -C /home/pi/seedGerminationMonitoring push)
 	let VAR=VAR-1
 	sleep 5s
