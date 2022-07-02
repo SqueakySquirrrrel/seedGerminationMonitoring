24c6fe55a0d4d87d0e26a998c43a763cbad86830
Siddhrajsinh Padhiyar <siddhrajsinh.padhiyar@gmail.com>
2022-07-01 21:04:19 -0500

    Testing

--- Images/testScript.sh
+++ Images/testScript.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/bash
 VAR=5
+echo "22" > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio22/direction
+
 while [ $VAR -gt 0 ]; do
 	#echo Value of VAR is: $VAR	
+	echo "1" > /sys/class/gpio/gpio22/value
+	sleep 1s
 	$(raspistill -o $VAR.jpg)
+	echo "0" > /sys/class/gpio/gpio22/value
 	$(git -C /home/pi/seedGerminationMonitoring add --all)
-	$(git -C /home/pi/seedGerminationMonitoring commit -m "Image upload")
+	$(git -C /home/pi/seedGerminationMonitoring commit -m "Testing")
 	$(git -C /home/pi/seedGerminationMonitoring push)
 	let VAR=VAR-1
 	sleep 5s
